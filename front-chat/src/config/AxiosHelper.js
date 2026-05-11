db2eb870ab9ff3caeec52c17261fbc11f4ec29d4
mukul-nayak <mukul.nayak.19063@iitgoa.ac.in>
2026-05-11 21:26:04 +0530

    updated URLs

--- front-chat/src/config/AxiosHelper.js
+++ front-chat/src/config/AxiosHelper.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-export const baseURL = "http://localhost:8080";
+// export const baseURL = "http://localhost:8080";
+export const baseURL = "https://chatappbackend-w9o2.onrender.com";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
